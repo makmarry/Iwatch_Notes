819bb3d7c7f8e9ab6d3e94e6f3ec5d4970740ad4
Mohak Parmar <mohak@topsinfosolutions.com>
2021-08-05 15:09:59 +0530

    Initial Commit

---
+++ Iwatch_NotesApp WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Iwatch_NotesApp WatchKit Extension
+//
+//  Created by Mohak Parmar on 05/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Iwatch_NotesApp WatchKit Extension/Iwatch_NotesAppApp.swift
@@ -0,0 +1,19 @@
+//
+//  Iwatch_NotesAppApp.swift
+//  Iwatch_NotesApp WatchKit Extension
+//
+//  Created by Mohak Parmar on 05/08/21.
+//
+
+import SwiftUI
+
+@main
+struct Iwatch_NotesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
